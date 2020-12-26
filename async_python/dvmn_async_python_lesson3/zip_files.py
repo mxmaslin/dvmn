6e5a13c8703cd45816492f678caadcf9f5d11043
Maxim Maslin <zapzarap@yandex.ru>
2020-12-26 17:51:45 +0300

    zipfile -> zip

--- async_python/dvmn_async_python_lesson3/zip_files.py
+++ async_python/dvmn_async_python_lesson3/zip_files.py
@@ -1,10 +1,9 @@
-import os
-from os.path import basename
-from zipfile import ZipFile
+import subprocess
 
 
-with ZipFile('zipped.zip', 'w') as zip_obj:
-   for folder_name, subfolders, filenames in os.walk('files_to_zip'):
-       for filename in filenames:
-           file_path = os.path.join(folder_name, filename)
-           zip_obj.write(file_path, basename(file_path))
+process = subprocess.Popen(
+    ['zip', '-r', 'zipped.zip', 'files_to_zip'],
+    stdout=subprocess.PIPE, 
+    stderr=subprocess.PIPE
+)
+process.communicate()
