40940cbe38719b8e023b016d598506cd7d91f028
Maxim Maslin <zapzarap@yandex.ru>
2020-12-26 19:15:03 +0300

    upd

--- async_python/dvmn_async_python_lesson3/zip_files.py
+++ async_python/dvmn_async_python_lesson3/zip_files.py
@@ -2,7 +2,7 @@ import subprocess
 
 
 process = subprocess.Popen(
-    ['zip', '-r', 'zipped.zip', 'files_to_zip'],
+    ['zip', '-r', '-', 'files_to_zip'],
     stdout=subprocess.PIPE, 
     stderr=subprocess.PIPE
 )
